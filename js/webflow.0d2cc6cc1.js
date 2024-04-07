5ac1b046708721b7a5ee088bb2cd04e16a551378
Jeff Sanders <kepi.creative@gmail.com>
2024-04-06 21:52:44 -0600

    Update webflow.0d2cc6cc1.js

--- js/webflow.0d2cc6cc1.js
+++ js/webflow.0d2cc6cc1.js
@@ -1209,7 +1209,6 @@
         var doc = document;
         var $html = $("html");
         var $body = $("body");
-        var namespace = ".w-webflow-badge";
         var location = window.location;
         var isPhantom = /PhantomJS/i.test(navigator.userAgent);
         var fullScreenEvents = "fullscreenchange webkitfullscreenchange mozfullscreenchange msfullscreenchange";
@@ -1230,16 +1229,6 @@
         function onFullScreenChange() {
           var fullScreen = doc.fullScreen || doc.mozFullScreen || doc.webkitIsFullScreen || doc.msFullscreenElement || Boolean(doc.webkitFullscreenElement);
           $(brandElement).attr("style", fullScreen ? "display: none !important;" : "");
-        }
-        function createBadge() {
-          var $brand = $('<a class="w-webflow-badge"></a>').attr("href", "https://webflow.com?utm_campaign=brandjs");
-          var $logoArt = $("<img>").attr("src", "https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-icon-d2.89e12c322e.svg").attr("alt", "").css({
-            marginRight: "4px",
-            width: "26px"
-          });
-          var $logoText = $("<img>").attr("src", "https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-text-d2.c82cec3b78.svg").attr("alt", "Made in Webflow");
-          $brand.append($logoArt, $logoText);
-          return $brand[0];
         }
         function ensureBrand() {
           var found = $body.children(namespace);
